297d78267e015aa4a6f7ef846f1d90a4725273bd
이원빈 <ldcumg@naver.com>
2025-09-07 00:28:19 +0900

    feat: NativeWind 적용

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-500">테일윈드가 왜 안되냐</Text>
+    </View>
+  );
+};
+
+export default Index;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+// tailwind.config.js 예시
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: { extend: {} },
+  plugins: [],
+};
