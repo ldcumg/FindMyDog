9796601c85082992bc39043b47ca828a1137977b
이원빈 <ldcumg@naver.com>
2025-09-14 01:41:27 +0900

    feat: 탭 적용

---
+++ app/(tabs)/gallery.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <View>
+      <Text>gallery</Text>
+    </View>
+  )
+}
+
+export default Gallery
